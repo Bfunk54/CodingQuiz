2c9ebf19b188a6055438aadf7b46343a226ba09a
Bfunk54 <ben_fein@icloud.com>
2022-10-30 14:41:24 -0400

    updated attempt to get the highscores to be persistent

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -5,6 +5,8 @@ window.onload = function() {
     loadHighscores();
   };
 
+let highscoreArr = [];
+
 function loadHighscores() {
     // var newUl = document.createElement("ul");
     // newUl.id = "highScoresList";
@@ -13,19 +15,22 @@ function loadHighscores() {
     highscoreCount = 0
     highscoreLocal = 'highscores' + highscoreCount;
     let theHighscore = (localStorage.getItem(highscoreLocal));
-    highscoreCount++;
+   
     console.log(theHighscore);
     console.log(JSON.parse(theHighscore));
     let highscoreObj = JSON.parse(theHighscore);
 if(theHighscore){
     let newLi = document.createElement("li");
     let highscoreText = 'Name: ' + highscoreObj.name + '    Points Scored: ' + highscoreObj.pointsScored;
-    console.log(highscoreText);
+    console.log(JSON.stringify(highscoreText));
+    highscoreArr.push(JSON.stringify(highscoreText));
     // newLi.appendChild = (highscoreText);
+    console.log(highscoreArr);
+    newLi.textContent += highscoreArr[highscoreCount];
     console.log(newLi);
-    newLi.textContent += highscoreText;
     highScoreAdd.appendChild(newLi);
-    console.log(highScoreAdd);
+    highscoreCount++;
+    
     // localStorage.setItem("highscores", '');
 }else{
     console.log('No highscores yet');
